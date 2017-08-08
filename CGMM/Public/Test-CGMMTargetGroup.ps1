6bbbc549f213037e8cfeca90564713d10bb89d30
Rick-2CA <Rick-2CA@users.noreply.github.com>
2017-08-08 14:20:49 -0500

    Test-CGMMTargetGroup & CGMM.TargetGroup.Tests Second Edition

--- CGMM/Public/Test-CGMMTargetGroup.ps1
+++ CGMM/Public/Test-CGMMTargetGroup.ps1
@@ -4,135 +4,77 @@ Function Test-CGMMTargetGroup {
     Run Pester tests against a target group for migration.
 
     .DESCRIPTION
-    Run Pester tests against a target group for migration.  Accepts a group name or a group object (from Get-CGMMTargetGroup or the contents of an export from Get-CGMMTargetGroup).  A default script is executed if a Pester script is not defined with the Script parameter.
+    Run Pester tests against a target group for migration.  A default Pester script is executed to validate whether the group can be staged successfully.
 
     .EXAMPLE
     Test-CGMMTargetGroup -Identity $Identity
 
-    Run the default Pester test against an existing target group.  This method will run Get-CGMMTargetGroup to find the identity.
+    Run the default Pester test against an existing target group.
+    .EXAMPLE
+    Test-CGMMTargetGroup -Identity $Identity -Tag EXOnPrem 
+    
+    Use predefined Tags to run one or more tests.  Allows you to rerun portions of the test when correcting group configurations.
+    .EXAMPLE
+    Test-CGMMTargetGroup -Identity $Identity -PassThru 
+
+    The PassThru switch causes Invoke-Pester to produce an output object which can be analyzed by its caller, instead of only sending output to the console.
     .EXAMPLE
     Test-CGMMTargetGroup -Identity $Identity -DomainController $DomainController 
-    
-    Run the default Pester script specifying a domain controller for the Get-CGMMTargetGroup query.
-    .EXAMPLE
-    Test-CGMMTargetGroup -CGMMTargetGroupObject $CGMMTargetGroupObject -Tag MSOnline,EXOnline
 
-    Run the default Pester script with specified tags.  Tags can be run to test specifc groups of tests.    
-    .EXAMPLE
-    Test-CGMMTargetGroup -CGMMTargetGroupObject $CGMMTargetGroupObject -Script $Script
-
-    Run specified Pester script(s) against the CGMMTargetGroupObject.
+    Ensure on premise queries use a specified domain controller within the Pester test
     .NOTES
 
     #>
     #requires -Module Pester
 
-    [CmdletBinding(DefaultParameterSetName="Identity")]
+    [CmdletBinding()]
     param(
         # Pipeline variable
-        [Parameter(ParameterSetName='Identity',Mandatory=$true,Position=0)]
+        [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
         [string]$Identity,
-
-        [Parameter(ParameterSetName='CGMMTargetGroupObject',Mandatory=$true,Position=0)]
-        [ValidateNotNullOrEmpty()]
-        [object]$CGMMTargetGroupObject,
         
-        [Parameter(ParameterSetName='Identity')]
-        [Parameter(ParameterSetName='CGMMTargetGroupObject')]
-        [ValidateNotNullOrEmpty()]
-        [object[]]$Script=$DefaultPesterTest,
-        
-        [Parameter(ParameterSetName='Identity')]
-        [Parameter(ParameterSetName='CGMMTargetGroupObject')]
-        [string[]]$TestName,
-        
-        [Parameter(ParameterSetName='Identity')]
-        [Parameter(ParameterSetName='CGMMTargetGroupObject')]
+        [Parameter()]
+        [ValidateSet('EXOnline','EXOnPrem','MSOnline')]
         [string[]]$Tag,
 
-        [Parameter(ParameterSetName='Identity')]
-        [Parameter(ParameterSetName='CGMMTargetGroupObject')]
+        [Parameter()]
         [Switch]$PassThru,
                 
-        [Parameter(ParameterSetName='Identity')]
+        [Parameter()]
         [string]$DomainController
     )
 
 	begin {}
 	process	{
-        # Use the identity to create a group object to run Pester against
-		If ($PsCmdlet.ParameterSetName -eq 'Identity') {
-            Try {
-                $SavedErrorActionPreference = $Global:ErrorActionPreference
-                $Global:ErrorActionPreference = 'Stop'
+        # Use the default pester test path set in the PSM1
+        $Script = $DefaultPesterTest
 
-                # Splat for Get-CGMMTargetGroup
-                $getCGMMTargetGroupSplat = @{
-                    Identity = $Identity
-                    ErrorAction = 'Stop'
-                }
-
-                # Validate the Domain Controller
-                If ($PSBoundParameters.DomainController) {
-                    Write-Verbose "Validating domain controller $DomainController with Test-Connection"
-                    Test-Connection $DomainController -Count 2 -ErrorAction Stop | Out-Null
-                    $getCGMMTargetGroupSplat.Add('DomainController',$DomainController)
-                }
-
-                # Query target group
-                Write-Verbose "Querying $Identity"
-                $GroupObject = Get-CGMMTargetGroup @getCGMMTargetGroupSplat
-            }
-            Catch {
-                $PsCmdlet.ThrowTerminatingError($PSItem)
-            }
-            Finally {
-                $Global:ErrorActionPreference = $SavedErrorActionPreference
-            }
+        # Check for Exchange cmdlet availability in On Prem & Exchange Online
+        Try {Test-CGMMCmdletAccess -Environment OnPrem,Cloud -ErrorAction Stop}
+        Catch {
+            $PsCmdlet.ThrowTerminatingError($PSItem)
         }
 
-        # If GroupObject wasn't queried grab the CGMMTargetGroupObject
-        If ($null -eq $GroupObject -and $PSBoundParameters.CGMMTargetGroupObject) {
-            $GroupObject = $PSBoundParameters.CGMMTargetGroupObject
+        # Set Script Parameters
+        $ScriptParameters = @{
+            Identity   = $PSBoundParameters.Identity
+        }
+        If ($PSBoundParameters.DomainController) {
+            $ScriptParameters.Add('DomainController',$PSBoundParameters.DomainController)
         }
 
-        $invokePester = @{
-            Script  = @{Path = $Script; Parameters = @{GroupObject = $GroupObject}}
-        }
-        If ($PSBoundParameters.TestName) {
-            $invokePester.Add('TestName',$PSBoundParameters.TestName)
+        # Set Pester Parameters
+        $invokePesterSplat = @{
+            Script  = @{Path=$Script;Parameters=$ScriptParameters}
         }
         If ($PSBoundParameters.Tag) {
-            $invokePester.Add('Tag',$PSBoundParameters.Tag)
+            $invokePesterSplat.Add('Tag',$PSBoundParameters.Tag)
         }
         If ($PSBoundParameters.PassThru) {
-            $invokePester.Add('PassThru',$PSBoundParameters.PassThru)
+            $invokePesterSplat.Add('PassThru',$PSBoundParameters.PassThru)
         }
-        Invoke-Pester @invokePester
+        Invoke-Pester @invokePesterSplat
     }
 	end {}
 }
-
-<#
-Test-CGMMTargetGroup will need to be custom for the module.  It'll run two stages:
-    * Prem Tests
-    * Cloud Tests
-
-The Prem tests will ensure that Get-CGMMTargetGroup's Get-PremCGMM* calls are validated
-The Cloud tests will ensure that Get-CGMMTargetGroup's Get-CloudCGMM* calls are validated
-    Also tests MSOnline domains
-
-The function will not allow other tests.  Users will have to write and call their own tests for Invoke-Pester
-The function will not accept objects in leiu of Identity.
-The function should still accept tags, which can now use ValidateSet, and PassThru
-
-# Tests
-Get-PremCGMMDistributionGroup
-Get-PremCGMMDistributionGroupMember
-Get-CGMMDistributionGroupMembership $GroupObject.PrimarySmtpAddress (OnPrem)
-Get-CGMMDistributionGroupMembership $GroupObject.PrimarySmtpAddress -Cloud
-ForEach ($User in $Group.AcceptMessagesOnlyFrom) {Try {$null = Get-PremCGMMRecipient $User -ErrorAction Stop}Catch {$User}}
-Get-CloudCGMMDistributionGroup
-
-#>
--- CGMM/Tests/CGMM.TargetGroup.Tests.ps1
+++ CGMM/Tests/CGMM.TargetGroup.Tests.ps1
@@ -1,23 +1,138 @@
-Describe "CGMM MSOnline Tests" -Tag MSOnline {
-    # Perform tests that utilize MSOnline
-    Context MSOnline {
-        # Test Connectivity By Storing Msol Domains
-        Try {
-            $MsolDomains = Get-MsolDomain -ErrorAction Stop | Select-Object -Expand Name
-            It "MSOnline Domains Exist" {
-                $MsolDomains | Should not BeNullOrEmpty 
-            }
-            $Skip = @{}
+param (
+    $Identity,
+    $DomainController
+)
+
+Describe "CGMM Exchange On-Premise Tests" -Tag EXOnPrem {
+    # Store and set EAP Value
+    $EAPSaved = $Global:ErrorActionPreference
+    $Global:ErrorActionPreference = 'Stop'
+
+    # Validate Users
+    Context 'Validate Members Exist in Exchange On-Premise' {
+        $Skip = @{}
+        $SkipThisContext = $False
+
+        # Get the 'raw' distribution group properties
+        $getPremCGMMDistributionGroupSplat = @{
+            Identity    = $Identity
+            ErrorAction = 'Stop'
         }
-        Catch {
-            It "Connect-MsolService Executed" {
-                $False | Should Be $True
+        If ($null -ne $DomainController) {
+            $getPremCGMMDistributionGroup.Add('DomainController',$DomainController)
+        }
+        Try {$DistributionGroup = Get-PremCGMMDistributionGroup @getPremCGMMDistributionGroupSplat}
+        Catch {$SkipThisContext = $True}
+
+        It 'Distribution Group Query Successful' {
+            $DistributionGroup.Name | Should Be $Identity
+        }
+
+        # Check properties that require members to exist in Exchange On-Premise
+        $PropertiesToCheck = @(
+            'AcceptMessagesOnlyFromSendersOrMembers','BypassModerationOnlyFromSendersOrMembers',
+            'GrantSendOnBehalfTo','ManagedBy','ModeratedBy','RejectedSendersSendersOrMembers'
+        )
+        ForEach ($Property in $PropertiesToCheck) {
+            $Skip = @{}
+            $PropertyCount = $DistributionGroup.$Property.count
+            If ($SkipThisContext -eq $True -or $PropertyCount -eq 0) {
+                $Skip = @{Skip = $True}
             }
+            It "All $PropertyCount members in property $Property are available in Exchange On-Premise" @Skip {
+                $CaughtMembers = $null
+                $CaughtMembers = ForEach ($Member in $DistributionGroup.$Property) {
+                    $getPremCGMMRecipientSplat = @{
+                        Identity = $Member
+                    }
+                    If ($null -ne $DomainController) {
+                        $getPremCGMMDistributionGroup.Add('DomainController',$DomainController)
+                    }
+                    Try {$null = Get-PremCGMMRecipient @getPremCGMMRecipientSplat}
+                    Catch {
+                        $Member
+                    }
+                }
+                $CaughtMembers | Should BeNullOrEmpty
+            }
+        }
+
+        # Look for DistributionGroupMembers that aren't cloud supported recipient types
+        $getPremCGMMDistributionGroupMemberSplat = @{
+            Identity    = $Identity
+            ErrorAction = 'Stop'
+        }
+        If ($null -ne $DomainController) {
+            $getPremCGMMDistributionGroup.Add('DomainController',$DomainController)
+        }
+        Try {$DistributionGroupMembers = Get-PremCGMMDistributionGroupMember @getPremCGMMDistributionGroupMemberSplat}
+        Catch {$SkipThisContext = $True}
+
+        $SupportedRecipientTypes = @(
+            'DynamicDistributionGroup','MailContact','MailNonUniversalGroup',
+            'MailUniversalDistributionGroup','MailUniversalSecurityGroup',
+            'MailUser','PublicFolder','UserMailbox'
+        )
+
+        It 'Distribution Group Member Type Should Be Supported In Cloud' @Skip {
+            $NotSupported = ForEach ($Member in $DistributionGroupMembers) {
+                If ($SupportedRecipientTypes -notcontains $Member.RecipientType) {
+                    $Member.Name
+                }
+            }
+            $NotSupported | Should BeNullOrEmpty
+        }
+
+        It 'Distribution Group is Synced to Exchange Online' @Skip {
+            $CloudGroupQuery = Get-CloudCGMMDistributionGroup -Identity $Identity -ErrorAction Stop
+            $CloudGroupQuery.Name | Should Be $Identity
+        }
+    }
+    # Return EAP Value
+    $Global:ErrorActionPreference = $EAPSaved
+}
+
+Describe "CGMM MSOnline Tests" -Tag MSOnline {
+    # Store and set EAP Value
+    $EAPSaved = $Global:ErrorActionPreference
+    $Global:ErrorActionPreference = 'Stop'
+
+    # Query the distribution group for the EmailAddresses property
+    $getPremCGMMDistributionGroupSplat = @{
+        Identity    = $Identity
+        ErrorAction = 'Stop'
+    }
+    If ($null -ne $DomainController) {
+        $getPremCGMMDistributionGroup.Add('DomainController',$DomainController)
+    }
+    Try {$EmailAddresses = Get-PremCGMMDistributionGroup @getPremCGMMDistributionGroupSplat | Select-Object -ExpandProperty EmailAddresses}
+    Catch {}
+
+    # Return EAP Value
+    $Global:ErrorActionPreference = $EAPSaved
+
+    Context MSOnline {
+        # Get a list of MsolDomains from MSOnline
+        $Skip = @{}
+        If ($null -eq $EmailAddresses) {
             $Skip = @{Skip = $True}
+        }
+        Try {
+            [array]$MsolDomains = Get-MsolDomain -ErrorAction Stop | Select-Object -Expand Name
+        }
+        Catch {$Skip = @{Skip = $True}}
+
+        # Show tests regarding the MSOnline query
+        It "MSOnline Successfully Queried" {
+            $Skip.Skip | Should Not Be $True
+        }
+
+        It "Found $($MsolDomains.count) MsolDomains" @Skip {
+            $MSolDomains | Should Not BeNullOrEmpty
         }
 
         # Validate all assigned SMTP addresses are valid Msol domains
-        [array]$SMTPAddresses = $GroupObject.EmailAddresses | Where-Object {$_ -match '^SMTP:'}
+        [array]$SMTPAddresses = $EmailAddresses | Where-Object {$_ -match '^SMTP:'}
         [array]$Domains = ForEach ($Address in $SMTPAddresses) {
             $Address.Split("@")[1]
         }
@@ -35,25 +150,18 @@ Describe "CGMM Exchange Online Tests" -Tag EXOnline {
     $EAPSaved = $Global:ErrorActionPreference
     $Global:ErrorActionPreference = 'Stop'
 
-    Context 'Validate access to Exchange Online' {
-        It 'Cloud CGMM Cmdlet Access Does Not Throw Error' {
-            {Test-CGMMCmdletAccess -Environment Cloud} | Should Not Throw
-        }
-    }
-
     # Validate Users
-    Context 'Validate Members Exist in Exchange Online'{
+    Context 'Validate Members Exist in Exchange Online' {
         $Skip = @{}
         $SkipThisContext = $False
 
-        # Perform a query to see if connectivity exists.  We don't want warnings or errors for this query
-        $getCloudCGMMRecipientSplat = @{
-            ResultSize = 1
-            WarningAction = 'SilentlyContinue'
-            ErrorAction = 'SilentlyContinue'
+        Try {
+            $GroupObject = Get-CGMMTargetGroup -Identity $Identity -DomainController $DomainController
         }
-        If ($null -eq (Get-CloudCGMMRecipient @getCloudCGMMRecipientSplat)) {
-            $SkipThisContext = $True
+        Catch {$SkipThisContext = $True}
+
+        It "Get-CGMMTargetGroup successfully queries" {
+            $GroupObject | Should Not BeNullOrEmpty
         }
 
         # Check properties that require members to exist in Exchange Online
