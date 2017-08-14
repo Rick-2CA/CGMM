e682e409d6ab9018f7583606350d1cc9c97602e9
Rick-2CA <Rick-2CA@users.noreply.github.com>
2017-08-14 15:51:21 -0500

    build.ps1 patch to avoid PowerShellGet v1.1.3.2 due to a deployment bug impacting this project [skip ci]

--- Build/build.ps1
+++ Build/build.ps1
@@ -11,6 +11,18 @@ param ($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
+# PowerShellGet Work Around
+## v1.1.3.2 issue #156 (https://github.com/PowerShell/PowerShellGet/issues/156) impacts this project.
+## The work around below will hopefully allow PSGallery deployments to work until a new release is published.
+$PSGetVersions = Get-Module PowerShellGet -ListAvailable
+If ($PSGetVersions[0].Version -eq [System.Version]'1.1.3.2') {
+    If ($PSGetVersions[1].Version -ne [System.Version]'1.1.3.1') {
+        Find-Module PowerShellGet -RequiredVersion 1.1.3.1 | Install-Module
+    }
+    Remove-Module PowerShellGet -Force -ErrorAction SilentlyContinue
+    Import-Module -Name PowerShellGet -RequiredVersion 1.1.3.1
+}
+
 $Modules = @("Psake", "PSDeploy","BuildHelpers","PSScriptAnalyzer", "Pester","Posh-Git")
 
 ForEach ($Module in $Modules) {
