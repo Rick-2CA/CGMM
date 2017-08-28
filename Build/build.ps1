edb6281595c39b9372af9a979ef70f31c7466205
Rick-2CA <Rick-2CA@users.noreply.github.com>
2017-08-28 10:00:09 -0500

    Changed build code dealing with PowerShellGet to work around bug impacting PSGallery deployments [skip ci]

--- Build/build.ps1
+++ Build/build.ps1
@@ -14,14 +14,13 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 # PowerShellGet Work Around
 ## v1.1.3.2 issue #156 (https://github.com/PowerShell/PowerShellGet/issues/156) impacts this project.
 ## The work around below will hopefully allow PSGallery deployments to work until a new release is published.
-$PSGetVersions = Get-Module PowerShellGet -ListAvailable
-If ($PSGetVersions[0].Version -eq [System.Version]'1.1.3.2') {
-    If ($PSGetVersions[1].Version -ne [System.Version]'1.1.3.1') {
-        Find-Module PowerShellGet -RequiredVersion 1.1.3.1 | Install-Module
-    }
-    Remove-Module PowerShellGet -Force -ErrorAction SilentlyContinue
-    Import-Module -Name PowerShellGet -RequiredVersion 1.1.3.1
+## Edit 2:  AppVeyor returned v1.0.0.1.  Changed code below to try and force 1.1.3.1.
+$PowerShellGetv1131 = Get-Module PowerShellGet -ListAvailable | Where-Object {$_.Version -eq [System.Version]'1.1.3.1'}
+If ($null -eq $PowerShellGetv1131) {
+    Find-Module PowerShellGet -RequiredVersion 1.1.3.1 | Install-Module
 }
+Remove-Module PowerShellGet -Force -ErrorAction SilentlyContinue
+Import-Module -Name PowerShellGet -RequiredVersion 1.1.3.1
 
 $Modules = @("Psake", "PSDeploy","BuildHelpers","PSScriptAnalyzer", "Pester","Posh-Git")
 
