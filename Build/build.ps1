9c0f45efc98bd8763a860d88852b0a291a6bed83
Rick-2CA <Rick-2CA@users.noreply.github.com>
2018-06-15 12:02:19 -0500

    Build test !KeepVersion

--- Build/build.ps1
+++ Build/build.ps1
@@ -9,12 +9,18 @@ Warren F. (RamblingCookieMonster)
 param ($Task = 'Default')
 
 # Grab nuget bits, install modules, set build variables, start build.
+Write-Warning 'Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null'
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 # PowerShellGet Work Around
+Write-Warning 'Install-Module PowerShellGet'
 Find-Module PowerShellGet | Install-Module -Force -SkipPublisherCheck
+Write-Warning 'Import-Module -Name PowerShellGet -Force'
 Import-Module -Name PowerShellGet -Force
 
+Write-Warning 'Get-Module'
+Get-Module
+Write-Warning '-------------------------------'
 $Modules = @("Psake", "PSDeploy","BuildHelpers","PSScriptAnalyzer", "Pester","Posh-Git")
 
 ForEach ($Module in $Modules) {
